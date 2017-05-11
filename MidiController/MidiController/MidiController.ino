b63f6ba0cd2aa619151ac34d5cc7da2a1cf2714d
Jonas Wirth <jonas-wirth@gmx.ch>
2017-05-11 23:15:58 +0200

    Initial commit

---
+++ MidiController/MidiController/MidiController.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MidiController.ino
+ Created:	5/11/2017 11:14:13 PM
+ Author:	Jonas
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
